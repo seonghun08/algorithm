71525adfaf5379bc0c583d478a5aa79d6b819195
seonghun08 <shajh9708@naver.com>
2024-09-24 01:09:37 +0900

    feat: 06-08 ~ 06-09 결정알고리즘(이분탐색)

---
+++ 자바 알고리즘 문제풀이/6. Sorting, Searching/8. 이분검색/Main0608.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main0608 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(m, arr));
+    }
+
+    public static int solution(int m, int[] arr) {
+        Arrays.sort(arr);
+        int lt = 0, rt = arr.length;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) return mid + 1;
+            else if (arr[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
+        }
+        return -1;
+    }
+}
+
