98185e187d9404d60309039e2081d6ff3ddd41af
seonghun08 <shajh9708@naver.com>
2025-09-20 09:55:41 +0900

    refactor: 0206

--- 자바 알고리즘 문제풀이/2. Array/6. 뒤집은 소수/Main0206.java
+++ 자바 알고리즘 문제풀이/2. Array/6. 뒤집은 소수/Main0206.java
@@ -8,39 +8,45 @@ public class Main0206 {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int a : solution(arr)) {
+        List<Integer> answer = solution(n, arr);
+        for (int a : answer) {
             System.out.print(a + " ");
         }
     }
 
-    public static List<Integer> solution(int[] arr) {
+    public static List<Integer> solution(int n, int[] arr) {
         List<Integer> list = new ArrayList<>();
-        for (int n : arr) {
-            int num = reverse(n);
-            if (isPrime(num)) {
-                list.add(num);
+        boolean[] isPrime = getPrime(100_000);
+        for (int num : arr) {
+            int reverse = reverse(num);
+            if (isPrime[reverse]) {
+                list.add(reverse);
             }
         }
         return list;
     }
 
-    public static boolean isPrime(int num) {
-        if (num == 1) return false;
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                return false;
+    public static boolean[] getPrime(int size) {
+        boolean[] isPrime = new boolean[size + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= size; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= size; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return true;
+        return isPrime;
     }
 
-    public static int reverse(int n) {
-        int num = 0;
-        while (n > 0) {
-            int tmp = n % 10;
-            num = num * 10 + tmp;
-            n /= 10;
+    public static int reverse(int num) {
+        int rs = 0;
+        while (num > 0) {
+            int tmp = num % 10;
+            rs = rs * 10 + tmp;
+            num /= 10;
         }
-        return num;
+        return rs;
     }
 }
