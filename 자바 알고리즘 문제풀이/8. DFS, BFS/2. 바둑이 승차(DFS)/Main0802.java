3a280844c84b282933ce70c82bcba61ca6601fe8
seonghun08 <shajh9708@naver.com>
2025-12-18 23:01:03 +0900

    feat: 0802 dfs

--- 자바 알고리즘 문제풀이/8. DFS, BFS/2. 바둑이 승차(DFS)/Main0802.java
+++ 자바 알고리즘 문제풀이/8. DFS, BFS/2. 바둑이 승차(DFS)/Main0802.java
@@ -1,22 +1,6 @@
 import java.util.Scanner;
 
 public class Main0802 {
-
-    static int n, c;
-    static int max;
-
-    public static void dfs(int idx, int sum, int[] arr) {
-        if (sum > c) {
-            return;
-        }
-        if (idx == n) {
-            max = Math.max(max, sum);
-        } else {
-            dfs(idx + 1, sum + arr[idx], arr);
-            dfs(idx + 1, sum, arr);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         c = sc.nextInt();
@@ -26,6 +10,20 @@ public class Main0802 {
             arr[i] = sc.nextInt();
         }
         dfs(0, 0, arr);
-        System.out.print(max);
+        System.out.println(answer);
+    }
+
+    static int c, n;
+    static int answer = 0;
+
+    public static void dfs(int depth, int sum, int[] arr) {
+        if (sum > c) return;
+        if (depth == n) {
+            if (sum > answer) answer = sum;
+        } else {
+            dfs(depth + 1, sum + arr[depth], arr);
+            dfs(depth + 1, sum, arr);
+        }
     }
 }
+
