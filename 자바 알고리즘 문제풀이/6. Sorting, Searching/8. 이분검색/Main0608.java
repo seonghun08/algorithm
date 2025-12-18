18f7acb009a48c063130b17324fab89b21e4ee51
seonghun08 <shajh9708@naver.com>
2025-12-18 23:00:56 +0900

    feat: 0802 dfs

--- 자바 알고리즘 문제풀이/6. Sorting, Searching/8. 이분검색/Main0608.java
+++ 자바 알고리즘 문제풀이/6. Sorting, Searching/8. 이분검색/Main0608.java
@@ -18,9 +18,13 @@ public class Main0608 {
         int lt = 0, rt = arr.length;
         while (lt <= rt) {
             int mid = (lt + rt) / 2;
-            if (arr[mid] == m) return mid + 1;
-            else if (arr[mid] > m) rt = mid - 1;
-            else lt = mid + 1;
+            if (arr[mid] == m) {
+                return mid + 1;
+            } else if (arr[mid] > m) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
         }
         return -1;
     }
