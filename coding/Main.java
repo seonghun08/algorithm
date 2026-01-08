1a18def465353c433ce874c6ec84add7b7874fb2
seonghun08 <seonghun08@naver.com>
2026-01-08 16:56:49 +0900

    chore: etc

--- coding/Main.java
+++ coding/Main.java
@@ -1,24 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static int[][] dy = new int[34][34];
-
-    public static int dfs(int n, int r) {
-        if (dy[n][r] > 0) {
-            return dy[n][r];
-        }
-        if (n == r || r == 0) {
-            return 1;
-        }
-        return dy[n][r] = dfs(n - 1, r - 1) + dfs(n - 1, r);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int r = sc.nextInt();
-        int answer = dfs(n, r);
-        System.out.print(answer);
+
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        return answer;
     }
 }
