070fc7b7962374d81a893d2978cfd58892a1e4df
seonghun <68460507+seonghun08@users.noreply.github.com>
2026-04-09 13:24:04 +0900

    [level 1] Title: 노란불 신호등, Time: 10.71 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -2,52 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] signals) {
-        int time = 0;
-        int size = signals.length;
-        List<Signal> list = new ArrayList<>();
-        
-        for (int i = 0; i < size; i++) {
-            int signal[] = signals[i];
-            list.add(new Signal(
-                signal[0],
-                signal[0] + signal[1], 
-                signal[0] + signal[1] + signal[2]
-            ));
-        }
-        
-        // 모든 경우의 수가 안 맞는다면 어떻게 찾을 것인지?
-        while (time < 100_000_000) {
-            boolean flag = true;
-            for (Signal s : list) {
-                char curSignal = s.changeTime(time);
-                if (flag && curSignal != 'y') flag = false;
-            }
-            if (flag) return time;
-            time++;
-        }
+		int size = signals.length;
+		int[] cycles = new int[size];
+		int maxTime = 1;
+		
+		for (int i = 0; i < size; i++) {
+			cycles[i] = signals[i][0] + signals[i][1] + signals[i][2];
+			maxTime = lcm(maxTime, cycles[i]);
+		}
+		
+		for (int time = 1; time < maxTime; time++) {
+			boolean isYellow = true;
+			for (int i = 0; i < size; i++) {
+				int g = signals[i][0];
+				int y = signals[i][1];
+				int cycle = cycles[i];
+				int curTime = (time - 1) % cycle + 1;
+				if (curTime <= g || curTime > g + y) {
+					isYellow = false;
+					break;
+				}
+			}
+			if (isYellow) return time;
+		}
         return -1;
     }
-    
-    static class Signal {
-        
-        int g, y, r;
-        char cur = 'g'; // 최초는 초록불로 시작
-        
-        public char changeTime(int time) {
-            time %= r;
-            if (time <= g) {
-                return this.cur = 'g';
-            } 
-            if (time <= y) {
-                return this.cur = 'y';
-            }
-            return this.cur = 'r';
-        }
-        
-        public Signal(int g, int y, int r) {
-            this.g = g;
-            this.y = y;
-            this.r = r;
-        }
-    }
+	
+	private int gcd(int a, int b) {
+		if (b == 0) return a;
+		return gcd(b, a % b);
+	}
+	
+	private int lcm(int a, int b) {
+		return a / gcd(a, b) * b;
+	}
 }
