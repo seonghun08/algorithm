6f4712a92397b118515293bd4a6eb7683b1a0bb2
seonghun <68460507+seonghun08@users.noreply.github.com>
2026-07-06 20:15:00 +0900

    [level 1] Title: 노란불 신호등, Time: 6.26 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -3,36 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(int[][] signals) {
         int size = signals.length;
-		int[] cycles = new int[size];
-		int maxTime = 1;
-		
-		for (int i = 0; i < size; i++) {
-			cycles[i] = signals[i][0] + signals[i][1] + signals[i][2];
-			maxTime = lcm(maxTime, cycles[i]);
-		}
-		for (int time = 1; time < maxTime; time++) {
-			boolean isYellow = true;
-			for (int i = 0; i < size; i++) {
-				int g = signals[i][0];
-				int y = signals[i][1];
-				int cycle = cycles[i];
-				int curTime = (time - 1) % cycle + 1;
-				if (curTime <= g || curTime > g + y) {
-					isYellow = false;
-					break;
-				}
-			}
-			if (isYellow) return time;
-		}		
+        int[] cycles = new int[size];
+        int maxTime = 1;
+        
+        for (int i = 0; i < size; i++) {
+            cycles[i] = signals[i][0] + signals[i][1] + signals[i][2];
+            maxTime = lcm(maxTime, cycles[i]);
+        }
+        for (int time = 1; time <= maxTime; time++) {
+            boolean allYellow = true;
+            for (int i = 0; i < size; i++) {
+                int g = signals[i][0];
+                int r = signals[i][2];
+                int cycle = cycles[i];
+                int cur = (time - 1) % cycle + 1;
+                if (g >= cur || cur > cycle - r) {
+                    allYellow = false;
+                    break;
+                }
+            }
+            if (allYellow) return time;
+        }
         return -1;
     }
-	
-	private int gcd(int a, int b) {
-		if (b == 0) return a;
-		return gcd(b, a % b);
-	}
-	
-	private int lcm(int a, int b) {
-		return a / gcd(a, b) * b;
-	}
+    
+    private int lcm(int a, int b) {
+        return a / gcd(a, b) * b;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
 }
